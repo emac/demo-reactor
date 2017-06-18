e9c6e634ef0ca0938b65c937ebfde4f9547d4be1
Emac <shen.bin.1983@gmail.com>
2017-06-19 00:12:10 +0800

    create real BackpressureTests

--- src/test/java/cn/emac/demo/reactor/BackpressureTests.java
+++ src/test/java/cn/emac/demo/reactor/BackpressureTests.java
@@ -1,70 +1,85 @@
 package cn.emac.demo.reactor;
 
-import cn.emac.demo.reactor.domain.Restaurant;
-import cn.emac.demo.reactor.repositories.ReactiveRestaurantRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import reactor.core.Exceptions;
-import reactor.core.publisher.ConnectableFlux;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
 
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Emac
- * @since 2017-06-11
+ * @since 2017-06-19
  */
-public class BackpressureTests extends BaseTests {
+public class BackpressureTests {
+
+    private Flux<Integer> timerPublisher;
 
     @BeforeEach
-    public void beforeEach2() {
-        // initialize load
-        List<Restaurant> load = IntStream.range(0, 10)
-                .mapToObj(i -> new Restaurant("hello" + i, "hello" + i, "hello" + i))
-                .collect(Collectors.toList());
-        // insert
-        ReactiveRestaurantRepository.INSTANCE.insert(load).blockLast();
+    public void beforeEach() {
+        // initialize publisher
+        AtomicInteger count = new AtomicInteger();
+        timerPublisher = Flux.create(s ->
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        s.next(count.getAndIncrement());
+                        if (count.get() == 10) {
+                            s.complete();
+                        }
+                    }
+                }, 100, 100)
+        );
     }
 
     @Test
-    public void testNormandy() throws InterruptedException {
-        // findAll mimic Backpressure
+    public void testNormal() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
-        ConnectableFlux<Restaurant> connectableFlux = ReactiveRestaurantRepository.INSTANCE.findAll().publish();
-        _addConitinuousSubscriber(connectableFlux, latch);
-        connectableFlux.connect();
+        timerPublisher
+                .subscribe(r -> System.out.println("Continuous consuming " + r),
+                        e -> latch.countDown(),
+                        latch::countDown);
         latch.await();
     }
 
     @Test
     public void testBackpressure() throws InterruptedException {
-        // findAll mimic Backpressure
         CountDownLatch latch = new CountDownLatch(1);
-        ConnectableFlux<Restaurant> connectableFlux = ReactiveRestaurantRepository.INSTANCE.findAll().publish();
-        connectableFlux.doOnEach(r -> latch.countDown());
-        _addNappingSubscriber(connectableFlux);
-        _addConitinuousSubscriber(connectableFlux, latch);
-        connectableFlux.connect();
-        latch.await();
-    }
-
-    private void _addConitinuousSubscriber(ConnectableFlux<Restaurant> connectableFlux, CountDownLatch latch) {
-        connectableFlux.subscribe(r -> System.out.println("Continuous consuming " + r),
-                e -> latch.countDown(),
-                latch::countDown);
-    }
-
-    private void _addNappingSubscriber(ConnectableFlux<Restaurant> connectableFlux) {
-        connectableFlux.subscribe(r -> {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-                System.out.println("Napping consuming " + r);
-            } catch (InterruptedException e) {
-                Exceptions.propagate(e);
+        AtomicReference<Subscription> timerSubscription = new AtomicReference<>();
+        Subscriber<Integer> subscriber = new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                timerSubscription.set(subscription);
             }
-        });
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("consuming " + value);
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                latch.countDown();
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                latch.countDown();
+            }
+        };
+        timerPublisher.onBackpressureDrop().subscribe(subscriber);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                timerSubscription.get().request(1);
+            }
+        }, 100, 200);
+        latch.await();
     }
 }
